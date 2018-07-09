9f3d10e54f8f0c061912463d35a1ecb8efe5b1bf
Dwayne Wedderburn <dw2yn3@gmail.com>
2018-07-09 00:34:01 -0400

    Added array

--- Collection and Arrays/Program.cs
+++ Collection and Arrays/Program.cs
@@ -10,6 +10,28 @@ namespace Collection_and_Arrays
     {
         static void Main(string[] args)
         {
+            // Create a collection that is a list of strings
+
+            var dogs = new List<string>();
+
+            dogs.Add("Rock Wilder");
+            dogs.Add("Bulldog");
+            dogs.Add("Retriever");
+
+            // move through the list
+
+            //foreach (var dog in dogs)
+            //{
+            //    Console.WriteLine(dog+ " ");
+            //}
+
+            int[] a1 = new int[] { 1, 3, 5, 7, 9, 11 };
+
+            foreach (int num in a1)
+            {
+                Console.WriteLine(num + " ");
+            }
+            
         }
     }
 }
